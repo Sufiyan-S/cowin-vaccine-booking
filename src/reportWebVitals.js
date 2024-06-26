a03dd58b3e9fcd15f196e9a3bb7bbfb36c6b5cac
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2024-06-26 12:46:28 +0530

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -6,6 +6,7 @@
 
 
 
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
